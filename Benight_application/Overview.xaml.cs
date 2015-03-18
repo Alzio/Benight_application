e04e59e423c8087b369545ba2a9d03a1a7dd76c8
Guillaume <guillaume2.martin@epitech.eu>
2015-03-18 10:28:51 +0100

    First commit WP app
    1rst commit

---
+++ Benight_application/Overview.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Parse;
+
+namespace Benight_application
+{
+    public partial class Overview : PhoneApplicationPage
+    {
+        //public ParseUsersData parseUserDt;
+        public Overview()
+        {
+            InitializeComponent();
+
+            //var user = new ParseUser();
+            //string uName = user.Username;
+            //parseUserDt = new ParseUsersData { userPseudo = uName };
+            //this.DataContext = parseUserDt;
+        }
+
+        private void ButtonLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            ParseUser.LogOut();
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ Benight_application/ParseUsersData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benight_application
+{
+    class ParseUsersData
+    {
+        public string userPseudo {get; set; }
+        //public string userMail { get; set; }
+
+    }
+}
