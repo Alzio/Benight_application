e3f91523a72bdec4fa94a59e85c273889eb399c6
Guillaume <guillaume2.martin@epitech.eu>
2016-01-18 22:51:21 +0100

    Commit listparse.Object

--- Benight_application/ParseUsersData.cs
+++ Benight_application/ParseUsersData.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parse;
 
 namespace Benight_application
 {
-    class ParseUsersData
+    class MyParseUsersData
     {
+
         public string userPseudo {get; set; }
-        //public string userMail { get; set; }
+        public string userMail { get; set; }
+    }
+
+    class MyParseEventData
+    {
+
+        public string eventDesc { get; set; }
+        public string eventName { get; set; }
 
     }
 }
