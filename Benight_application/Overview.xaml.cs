86a5c94b447f7b13bca3f0a4fd395d2a93dda7f9
Guillaume <guillaume2.martin@epitech.eu>
2016-01-18 23:13:14 +0100

    Commit listparse.Object2

--- Benight_application/Overview.xaml.cs
+++ Benight_application/Overview.xaml.cs
@@ -5,25 +5,69 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Windows.Data;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Data.Linq;
 using Parse;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
 
 namespace Benight_application
 {
     public partial class Overview : PhoneApplicationPage
     {
-        //public ParseUsersData parseUserDt;
+        private MyParseUsersData parseUserData;
+        private MyParseEventData parseEventData;
+        public ObservableCollection<string> listEvent { get; set; }
+        public IEnumerable<ParseObject> ResultsEvent { get; set; }
+        List<string> _events = new List<string>();
+
         public Overview()
         {
+            DataContext = this;
+
+            
             InitializeComponent();
 
-            //var user = new ParseUser();
-            //string uName = user.Username;
-            //parseUserDt = new ParseUsersData { userPseudo = uName };
-            //this.DataContext = parseUserDt;
+            parseUserData = new MyParseUsersData { userPseudo = ParseUser.CurrentUser.Username.ToString()};
+            DataContext = parseUserData;
+
+            //ListEvent = App.CustomNavigationService.data as ObservableCollection<string>;
+
+            
+            List_Events();            
+            //parseEventData = new MyParseEventData { eventName = TodoEv.My_EventName };
         }
 
+
+        private async void List_Events()
+        {
+            //var queryEvent = ParseObject.GetQuery("Event");
+            //ResultsEvent = await queryEvent.FindAsync();
+
+            //EventListBox.ItemsSource = ResultsEvent;
+
+            var query = from name in ParseObject.GetQuery(TodoEv.ClassName)
+                        orderby name.CreatedAt
+                        select name;
+            ResultsEvent = await query.FindAsync();
+            EventListBox.ItemsSource = ResultsEvent;
+
+
+            //try
+            //{
+            //    var allItems = from item in await query.FindAsync()
+            //                   select new TodoEv(item);
+            //    var itemList = new ObservableCollection<TodoEv>(allItems);
+            //    EventListBox.ItemsSource = ResultsEvent;
+            //}
+            //catch (Exception e)
+            //{
+            //    MessageBox.Show("Erreur:\n" + e);
+            //}
+        }
         private void ButtonLogOut_Click(object sender, RoutedEventArgs e)
         {
             ParseUser.LogOut();
---
+++ Benight_application/ToDoEv.cs
@@ -0,0 +1,94 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benight_application
+{
+    public class TodoEv : INotifyPropertyChanged
+    {
+        public static readonly string ClassName = "Event";
+
+        public TodoEv() : this(new ParseObject(ClassName)) { }
+        public TodoEv(ParseObject backingObject)
+        {
+            if (backingObject.ClassName != ClassName)
+            {
+                throw new ArgumentException("Must create TodoItems with the proper ClassName");
+            }
+            //this.BackingObject = backingObject;
+            BackingObject = backingObject;
+        }
+
+        public ParseObject BackingObject { get; private set; }
+
+        public bool IsDirty
+        {
+            get
+            {
+                return BackingObject.IsDirty;
+            }
+        }
+
+        public string My_EventName
+        {
+            get
+            {
+                return BackingObject.ContainsKey("name") ? BackingObject.Get<string>("name") : null;
+                
+            }
+            set
+            {
+                if (value != My_EventName)
+                {
+                    BackingObject["name"] = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+        public string My_EvDescription
+        {
+            get
+            {
+                return BackingObject.ContainsKey("description") ? BackingObject.Get<string>("description") : null;
+            }
+            set
+            {
+                if (value != My_EvDescription)
+                {
+                    BackingObject["description"] = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var del = PropertyChanged;
+            if (del != null)
+            {
+                del(this, new PropertyChangedEventArgs(propertyName));
+                del(this, new PropertyChangedEventArgs("IsDirty"));
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await BackingObject.SaveAsync();
+            OnPropertyChanged("IsDirty");
+        }
+
+        public void Revert()
+        {
+            BackingObject.Revert();
+            OnPropertyChanged(String.Empty);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
